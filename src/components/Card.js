8e41072a7a4928f8881c3b942f28ca90281e1eb0
Papon Mateevorakulkij <furby153@gmail.com>
2023-07-07 14:46:02 +0700

    Change const Card to class Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,21 @@
 import React from "react";
 import './Card.css';
 
-const Card = ({ name, url, id}) => {
-    return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt='Sorry! Pic is unavailable for this Pokémon' width={200} height={200} />
-            <div>
-                <h2>{name}</h2>
-                <h5>PokéID: {id}</h5>
-                {/* <p>{url}</p> */}
+class Card extends React.Component {
+    render() {
+        const { name, url, id } = this.props;
+        
+        return (
+            <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt='Sorry! Pic is unavailable for this Pokémon' width={200} height={200} />
+                <div>
+                    <h2>{name}</h2>
+                    <h5>PokéID: {id}</h5>
+                    <p>{url}</p>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Card;
