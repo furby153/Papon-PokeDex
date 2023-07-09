31ca6509d63dd0108c612842d8956cc22ea6aaa7
Papon Mateevorakulkij <furby153@gmail.com>
2023-07-09 23:46:39 +0700

    "delete blank row"

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,7 +4,6 @@ const Stats = ( { stats } ) => {
     return (
         <div className='tl stats'>
             <h4>Base Status</h4>
-            
             {stats.map((stats, index) => ( //Loop for react instead of <p> {stats[i].stat.name} : {stats[i].base_stat} </p>
                 <p key={index}>
                     {stats.stat.name}: {stats.base_stat}
