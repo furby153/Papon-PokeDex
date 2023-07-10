729d20b4034d533f8ea5f408ed8879d3e0103b3d
Papon Mateevorakulkij <furby153@gmail.com>
2023-07-10 15:43:34 +0700

    add boarder to status

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Stats = ( { stats } ) => {
     return (
-        <div className='tl stats'>
+        <div className='tl ba b--blue br3 ph3 bg-lightest-blue stats'>
             <h4>Base Status</h4>
             {stats.map((stats, index) => ( //Loop for react instead of <p> {stats[i].stat.name} : {stats[i].base_stat} </p>
                 <p key={index}>
