f130145fc048b470fcf356f176df5cae5264e346
Papon Mateevorakulkij <furby153@gmail.com>
2023-07-14 18:06:29 +0700

    add Evolution method

--- src/components/EvolutionLeveling.js
+++ src/components/EvolutionLeveling.js
@@ -4,13 +4,38 @@ function EvolutionLeveling({ leveling }) {
   if (!leveling) {
     return null;
   }
-  const { min_level } = leveling;
-
-  return (
+    const evolutionType = leveling.trigger.name;
+  
+  if ( evolutionType === 'level-up') {
+    const { min_level } = leveling;
+    return (
+      <div>
+        <h4>Evolution method: {evolutionType.toUpperCase()}</h4>
+        <p className='evolutionCondition'>Minimum Level: {min_level}</p>
+      </div>
+    );
+  } else if (evolutionType === 'trade') {
+    return (
     <div>
-      <p>Minimum Level: {min_level}</p>
+      <h4>Evolution method: {evolutionType.toUpperCase()}</h4>
     </div>
-  );
+    );
+  } else if (evolutionType === 'use-item') {
+    const evolutionItem = leveling.item.name;
+    return (
+      <div>
+        <h4>Evolution method: {evolutionType.toUpperCase()}</h4>
+        <p className='evolutionCondition'>Item: {evolutionItem}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h4>Evolution method: {evolutionType.toUpperCase()}</h4>
+      </div>
+    );
+  }
+  
 }
 
 export default EvolutionLeveling;
