b6e4457ee8e92da2c29e504d1283ffd0270c7762
Papon Mateevorakulkij <furby153@gmail.com>
2023-07-26 18:44:39 +0700

    Rewrite Casd.js to much more cleaner

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './Card.css';
 import Details from "./Details";
-import pokeball from "../images/pokeball.png"
+import pokeball from "../images/pokeball.png";
 
 class Card extends React.Component {
     constructor(props) {
@@ -9,72 +9,69 @@ class Card extends React.Component {
         this.state = {
             showDetails: false,
             imageError: false,
-            // firstClick: true, // Add firstClick state to track if it's the first click on the card
         };
     }
 
+    // Toggle the showDetails state on card click
     handleClick = () => {
-        const { showDetails} = this.state;
+        const { showDetails } = this.state;
         if (!showDetails) {
-            this.setState({
-                showDetails: true,
-            });
+            this.setState({ showDetails: true });
         }
     };
-    
+
+    // Toggle the showDetails state on the "Show Details" button click
     handleShowDetailsClick = () => {
         this.setState((prevState) => ({
             showDetails: !prevState.showDetails,
         }));
     };
 
+    // Handle image loading error by showing the alternate image
     handleImageError = () => {
         this.setState({ imageError: true });
     };
 
+    // Generate the image URL for the given Pokemon ID
+    getImageUrl = (id) => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
     render() {
         const { name, url, id } = this.props;
         const { showDetails, imageError } = this.state;
         const widthHeight = 200;
-        
+
         return (
-            <div 
+            <div
                 className={`tc bg-light-green dib br3 pa3 ma2 ${showDetails ? '' : 'grow'} ${showDetails ? '' : 'onCardHover'} bw2 shadow-5`}
-                onClick={this.handleClick} // Add the click event handler
+                onClick={this.handleClick}
             >
-                {imageError ? ( // Render alternate image if there was an error
-                    <img
-                        src={pokeball} //
-                        alt='Sorry! Pic is unavailable for this Pokémon'
-                        width={widthHeight}
-                        height={widthHeight}
-                    />
-                ) : (
-                    <img
-                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-                        alt='Sorry! Pic is unavailable for this Pokémon'
-                        width={widthHeight}
-                        height={widthHeight}
-                        onError={this.handleImageError} // Add the onError event handler
-                    />
-                )}
+                {/* Render the appropriate image based on imageError */}
+                <img
+                    src={imageError ? pokeball : this.getImageUrl(id)}
+                    alt='Sorry! Pic is unavailable for this Pokémon'
+                    width={widthHeight}
+                    height={widthHeight}
+                    onError={this.handleImageError}
+                />
                 <div>
-                    <h2>{name.charAt(0).toUpperCase()+name.slice(1)}</h2>
+                    <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
                     <h5>PokéID: {id}</h5>
                 </div>
                 <div>
-                    {!showDetails && <p>Click to show details</p>}
-                    {
-                        showDetails && 
-                        <button 
-                            className={`showDetailsButton ${showDetails ? "redHover" : "greenHover"}`}
-                            onClick={this.handleShowDetailsClick}
-                        >
-                            {showDetails ? "Hide Details" : "Click to show details"}
-                        </button>
-                    }
-                    {showDetails && <Details url={url} />}
+                    {/* Conditional rendering for the "Show Details" button and Details component */}
+                    {showDetails ? (
+                        <>
+                            <button
+                                className={`showDetailsButton ${showDetails ? "redHover" : "greenHover"}`}
+                                onClick={this.handleShowDetailsClick}
+                            >
+                                Hide Details
+                            </button>
+                            <Details url={url} />
+                        </>
+                    ) : (
+                        <p>Click to show details</p>
+                    )}
                 </div>
             </div>
         );
